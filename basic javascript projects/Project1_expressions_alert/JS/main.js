2e2c7f3a6cd4c441ec06693002570d77a46c17b5
jackie-may <jackielynnmay@gmail.com>
2020-11-04 12:57:34 -0700

    expressions and alert practice
    Utilize the window.alert() and document.write() methods
    
    Assign string values to two variables and concatenate the variables together
    
    Assign a color to at least one variable using the fontcolor() method
    
    Write an expression
    
    Add comments throughout your JavaScript explaining your code

---
+++ basic javascript projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert ("Hello World");	//asking index.html to display a window alert that says "This page says Hello World"
+
+var family = "The May's", dad = "Russ", mom = "Dedie", daughter = "Jackie";	// assigning variables to the different people in this family
+
+var family = family.fontcolor("blue");	//assigning the font color blue to the family variable
+var dad = dad.fontcolor("green");	//assigning the font color green to the variable dad
+var mom = mom.fontcolor("pink");	//assigning the font color pink to the variable mom
+var daughter = daughter.fontcolor("orange");	//assigning the font color orange to the variable daughter
+
+document.write (family + " have " + (1+1+1) + " people in their family." + "Their names are " + dad + ", " + mom + ", and " + daughter + ".");	//asking index.html to display the variable family, perform the expression 1+1+1, and concatenate the several parts
